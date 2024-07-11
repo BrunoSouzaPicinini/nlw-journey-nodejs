1dd2b13239689bd002f33701cb021a2e116dd73c
Bruno de Souza Picinini <brunosouzapicinini@gmail.com>
2024-07-10 22:47:00 -0300

    add routes

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,13 @@
+import cors from '@fastify/cors';
 import fastify from 'fastify';
 import { serializerCompiler, validatorCompiler } from 'fastify-type-provider-zod';
+import { confirmParticipant } from './routes/confirm-participant';
 import { confirmTrip } from './routes/confirm-trip';
+import { createActivities } from './routes/create-activitie';
 import { createTrip } from './routes/create-trip';
-import cors from '@fastify/cors';
+import { getActivities } from './routes/get-activities';
+import { createLink } from './routes/create-link';
+import { getLinks } from './routes/get-links';
 
 const app = fastify();
 
@@ -17,6 +22,11 @@ app.setSerializerCompiler(serializerCompiler);
 
 app.register(createTrip);
 app.register(confirmTrip);
+app.register(confirmParticipant);
+app.register(createActivities);
+app.register(getActivities);
+app.register(createLink);
+app.register(getLinks);
 
 app.listen({ port: 3333 }).then(() => {
     console.log('Server is running on port 3333');
